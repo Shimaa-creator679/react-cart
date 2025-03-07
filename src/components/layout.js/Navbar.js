6080c9071fde915fcb46a94df27844876ff53e59
unknown <you@example.com>
2025-03-07 22:42:33 +0200

    هنهعخ

--- src/components/layout.js/Navbar.js
+++ src/components/layout.js/Navbar.js
@@ -3,36 +3,31 @@ import { Link } from 'react-router-dom'
 import { CartContext } from '../../store/cartcontext'
 
 export default function Navbar() {
-  const cart=useContext(CartContext);
+  const cart = useContext(CartContext);
+
   return (
-    <nav className= "navbar navbar-expand-lg navbar-light bg-light">
-    <div className="container-fluid ">
-      <a
-      className="navbar-brand" href='u7'>Navbar</a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="u7">Navbar</a>
 
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/products" className="nav-link" >products</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/cart" className="nav-link"  >carts<span className='badge bg-warning'>{cart.count}</span></Link>
-          </li>
-        
-       
-    </ul>
-       </div>     
-     
-
-   
-    </div>
-  </nav>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/products" className="nav-link">Products</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/cart" className="nav-link">Carts <span className="badge bg-warning">{cart.count}</span></Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
