ad01cf12f619ee6e1cc9749f6a78d9637f1cccb7
unknown <you@example.com>
2025-03-07 22:56:49 +0200

    غغغ

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';  // يجب استيراد الـ JS الخاص بـ Bootstrap مع Popper
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
